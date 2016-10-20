36c52277e84ff354ceecc5f7582968e61950316c
ppypenko <ppypenko@student.neumont.edu>
2016-10-20 08:17:00 -0600

    Added sounds

--- assets/scripts/game_enemy.js
+++ assets/scripts/game_enemy.js
@@ -1,63 +1,75 @@
 "use strict";
 var enemy = {
-    mothershipUfo: {},
-    mothershipShot: {},
     mothershipUfo: {
         ufo: {},
         shot: {},
         shotClones: [],
         speed: 5,
         health: 50,
-        shotSpeed: 10
+        shotSpeed: 10,
+        shotDamage: 5
     },
-    mainUfos: {
+    battlerUfo: {
         ufo: {},
         shot: {},
         ufoClones: [],
         shotClones: [],
+        cloneSize: 200,
         ufoSpeed: 5,
         health: 1,
         shotSpeed: 10,
         shotDamage: 1
     },
-    speederUfos: {
+    speederUfo: {
         ufo: {},
         shot: {},
         ufoClones: [],
         shotClones: [],
-        speed: 10,
+        cloneSize: 100,
+        ufoSpeed: 10,
         health: 3,
         shotSpeed: 15,
         shotDamage: 2
     }
-}
+};
 
 function buildEnemies() {
 
 }
 
 function buildMothershipUfo() {
-    enemy.mothershipUfo = new createjs.Bitmap(queue.getResult("mothership"));
-    enemy.mothershipUfo.regX = enemy.mothershipUfo.getBounds().width / 2;
-    enemy.mothershipUfo.regY = enemy.mothershipUfo.getBounds().height / 2;
-    enemy.mothershipUfo.visible = false;
+    enemy.mothershipUfo.ufo = new createjs.Bitmap(queue.getResult("mothership"));
+    enemy.mothershipUfo.ufo.regX = enemy.mothershipUfo.ufo.getBounds().width / 2;
+    enemy.mothershipUfo.ufo.regY = enemy.mothershipUfo.ufo.getBounds().height / 2;
+    enemy.mothershipUfo.ufo.visible = false;
 }
 
 function buildSpeederUfo() {
-    enemy.mothershipUfo = new createjs.Bitmap(queue.getResult("mothership"));
-    enemy.mothershipUfo.regX = enemy.mothershipUfo.getBounds().width / 2;
-    enemy.mothershipUfo.regY = enemy.mothershipUfo.getBounds().height / 2;
-    enemy.mothershipUfo.visible = false;
+    enemy.speederUfo.ufo = new createjs.Bitmap(queue.getResult("speeder"));
+    enemy.speederUfo.ufo.regX = enemy.speederUfo.ufo.getBounds().width / 2;
+    enemy.speederUfo.ufo.regY = enemy.speederUfo.ufo.getBounds().height / 2;
+    enemy.speederUfo.ufo.visible = false;
 }
 
-function buildMainUfo() {
-
+function buildBattlerUfo() {
+    enemy.battlerUfo.ufo = new createjs.Bitmap(queue.getResult("battler"));
+    enemy.battlerUfo.ufo.regX = enemy.battlerUfo.ufo.getBounds().width / 2;
+    enemy.battlerUfo.ufo.regY = enemy.battlerUfo.ufo.getBounds().height / 2;
+    enemy.battlerUfo.ufo.visible = false;
 }
 
 function cloneSpeederUfo() {
-
+    var i = 0;
+    for (i = 0; i < enemy.speederUfo.cloneSize; i += 1) {
+        enemy.speederUfo.ufoClones.push(enemy.speederUfo.ufo.clone());
+        stage.addChild(enemy.speederUfo.ufoClones[i]);
+    }
 }
 
-function cloneMainUfo() {
-
+function cloneBattlerUfo() {
+    var i = 0;
+    for (i = 0; i < enemy.battlerUfo.cloneSize; i += 1) {
+        enemy.battlerUfo.ufoClones.push(enemy.battlerUfo.ufo.clone());
+        stage.addChild(enemy.battlerUfo.ufoClones[i]);
+    }
 }
--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 "use strict";
 var stage, queue;
 var manifest = [{
-        src: "scripts/game_buttons.js"
+    src: "scripts/game_buttons.js"
 }, {
-        src: "scripts/game_enemy.js"
+    src: "scripts/game_enemy.js"
 }, {
-        src: "scripts/game_engine.js"
+    src: "scripts/game_engine.js"
 }, {
-        src: "scripts/game_keys.js"
+    src: "scripts/game_keys.js"
 }, {
-        src: "scripts/game_mouse.js"
+    src: "scripts/game_mouse.js"
 }, {
-        src: "scripts/game_player.js"
+    src: "scripts/game_player.js"
 }, {
-        src: "scripts/game_screen_credits.js"
+    src: "scripts/game_screen_credits.js"
 }, {
-        src: "scripts/game_screen_instructions.js"
+    src: "scripts/game_screen_instructions.js"
 }, {
-        src: "scripts/game_screen_levels.js"
+    src: "scripts/game_screen_levels.js"
 }, {
-        src: "scripts/game_screen_menu.js"
+    src: "scripts/game_screen_menu.js"
 }, {
-        src: "scripts/game_screen_over.js"
+    src: "scripts/game_screen_over.js"
 }, {
-        src: "scripts/game_screen_victory.js"
+    src: "scripts/game_screen_victory.js"
 }, {
-        src: "scripts/game_sound.js"
-},
-    {
-        src: "scripts/game_state.js"
+    src: "scripts/game_sound.js"
 }, {
-        src: "images/battler.png",
-        id: "battler"
+    src: "scripts/game_state.js"
 }, {
-        src: "images/mothership.png",
-        id: "mothership"
+    src: "images/battler.png",
+    id: "battler"
 }, {
-        src: "images/speeder.png",
-        id: "speeder"
+    src: "images/mothership.png",
+    id: "mothership"
+}, {
+    src: "images/speeder.png",
+    id: "speeder"
 }];
 
 function setupCanvas() {
